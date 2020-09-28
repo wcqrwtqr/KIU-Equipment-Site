6320840276b04e2f9afdd71c367557485c1bb1aa
wcqrwtqr <mohalbatati@hotmail.com>
2020-09-28 23:19:05 +0300

    `first commit with adding some apps and not able to run the first page successfully

---
+++ FAR/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FarConfig(AppConfig):
+    name = 'FAR'
---
+++ WTCDataBase/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+
+
+class HomePage(TemplateView):
+    template_name = 'index.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
