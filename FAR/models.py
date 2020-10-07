1a30a44cff51991e04f77b066041efe0f548af08
wcqrwtqr <mohalbatati@hotmail.com>
2020-10-07 21:16:59 +0300

    here in this branch will try to update the database and fill it with data

--- FAR/models.py
+++ FAR/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FAR_DB(models.Model):
+    asset_num = models.CharField(max_length=200,unique=True, blank=False)
+    serial_num = models.CharField(max_length=200,unique=True,blank=True)
+    description = models.CharField(max_length=800)
+    BU = models.CharField(max_length=4)
+    BL = models.CharField(max_length=4)
+    asset_life = models.IntegerField()
+    acquisition_cost  = models.DecimalField(max_digits=10, decimal_places=2)
+    tot_dep_value  = models.DecimalField(max_digits=10, decimal_places=2)
+    nbv  = models.DecimalField(max_digits=10, decimal_places=2)
+    acquisition_date  = models.DateField()
+    equ_type = models.CharField(max_length=200)
+    temp_location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return (self.serial_num , " " , self.equ_type)
+
+
