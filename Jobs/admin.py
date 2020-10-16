280338b741b43c23117328b6eece0170323e3582
wcqrwtqr <mohalbatati@hotmail.com>
2020-10-16 17:24:43 +0300

    Updated the Jobs app model with another related table of FAR equipment and added the H2S and Co2 fields, need more tweaking and more update to match the requriments, did not do any change to the view so far

--- Jobs/admin.py
+++ Jobs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import JobsDB
+from .models import JobsDB, JobMasterInfo
 # Register your models here.
 
 admin.site.register(JobsDB)
+admin.site.register(JobMasterInfo)
 
--- Jobs/models.py
+++ Jobs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from FAR.models import FAR_DB
 # Create your models here.
 
 class JobsDB(models.Model):
@@ -22,3 +23,17 @@ class JobsDB(models.Model):
         return '%s %s %s'% (self.JOBID, self.client, self.well)
 
 
+class JobMasterInfo(models.Model):
+    job = models.ForeignKey(JobsDB,null=True, on_delete=models.CASCADE)
+    assets = models.ManyToManyField(FAR_DB,null=True )
+    h2s = models.DecimalField(max_digits=3,decimal_places=2,blank=True)
+    co2 = models.DecimalField(max_digits=3,decimal_places=2,blank=True)
+    oilRate = models.DecimalField(max_digits=10,decimal_places=2,blank=True)
+    gasRate = models.DecimalField(max_digits=10,decimal_places=3,blank=True)
+    description = models.CharField(max_length= 800, blank=True)
+
+    def get_absolute_url(self):
+        return revere('jobs_master_info',kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return '%s'% (self.job )
