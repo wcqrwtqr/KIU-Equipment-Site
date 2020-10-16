d7de4590becdd1c1c92d7d743c16fe94aa433e65
wcqrwtqr <mohalbatati@hotmail.com>
2020-10-17 00:27:26 +0300

    This build is pre-complete for the Jobs many to many model and html view, managed to do another html page that shows the infomration from the jobs info and still did not manage to show the list of equipment in the view -- still needs work

--- Jobs/urls.py
+++ Jobs/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.JobsHomePage.as_view(), name='jobs'),
     path('jobs/<int:pk>', views.JobsDetailView.as_view(), name='jobs_detail'),
+    path('jobs/master_info/<int:pk>', views.JobsMasterInfoView.as_view(), name='jobs_master_info'),
 ]
--- Jobs/views.py
+++ Jobs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView,ListView,DetailView, View
-from .models import JobsDB
+from .models import JobsDB, JobMasterInfo
 # Create your views here.
 
 
@@ -10,6 +10,11 @@ class JobsHomePage(ListView):
     context_object_name = 'jobs_all'
 
 class JobsDetailView(DetailView):
-    template_name = 'Jobs/jobs_detail.html'
     queryset = JobsDB.objects.all()
     context_object_name = 'jobs_detail'
+    template_name = 'Jobs/jobs_detail.html'
+
+class JobsMasterInfoView(DetailView):
+    queryset = JobMasterInfo.objects.all()
+    context_object_name = 'jobs_master_info'
+    template_name = 'Jobs/jobs_master_info.html'
